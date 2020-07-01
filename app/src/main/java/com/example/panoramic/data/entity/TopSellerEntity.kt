2ccd426f59e02c20fe194d71fabd68030d906727
spr021 <saber.pourrahimi.1999@gmail.com>
2020-07-01 19:20:04 +0430

    add phone input xml layout

---
+++ app/src/main/java/com/example/panoramic/data/entity/TopSellerEntity.kt
@@ -0,0 +1,10 @@
+package com.example.panoramic.data.entity
+
+data class TopSellerEntity(
+    val name: String,
+    val totalScore: Int,
+    val shopName: String,
+    val state: String,
+    val profileImg: String,
+    val rank: Int
+)
